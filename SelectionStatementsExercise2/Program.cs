bb27aedab5ad478d4746f481fffc724f10051a67
Michael Berg <mberg12@bu.edu>
2023-04-12 12:54:36 -0400

    Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+
+
+            Console.WriteLine("What is your favorite subject in school?");
+            var subject = (Console.ReadLine());
+
+            switch (subject)
+            {
+                case "History":
+                    Console.WriteLine("I dont't like History");
+                    break;
+                case "Science":
+                    Console.WriteLine("I think Science is interesting.");
+                    break;
+                case "English":
+                    Console.WriteLine("I think English is boring.");
+                    break;
+                case "Gym":
+                    Console.WriteLine("I think Gym is fun.");
+                    break;
+                case "Math":
+                    Console.WriteLine("I think Math is confusing.");
+                    break;
+                default:
+                    Console.WriteLine("I haven't taken that subject before.");
+                    break;
+            }
+
         }
     }
 }
